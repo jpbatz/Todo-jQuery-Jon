fb3ec6ddfcd5d38b4c4c2e9ba840593b26ea83f4
Te Vallee <astronomemusic@gmail.com>
2014-12-04 21:25:30 -1000

    checkbox event listener added

--- public/js/todo.js
+++ public/js/todo.js
@@ -3,9 +3,19 @@ $( document ).ready(function() {
     if( event.which == 13 ) {
       event.preventDefault();
       var user_input = $('#target').val();
+      var checkbox = $('<input type="checkbox">');
       $('ul').append('<li>' + user_input + '</li>');
+      var checkbox = $('<input type="checkbox">');
+      $('li').append(checkbox);
       // resets to placeholder
       $('input').val('').removeAttr('checked').removeAttr('selected');
     }
+    // CROSS OUT LIST ITEM IF BOX CHECKED
+    $('input:checkbox').change(
+      function() {
+        if(this.checked) {
+          console.log("meow");
+        }
+      })
   });  
 });
