02e45bd782f375c3cb93f405d64e39018463da2b
Te Vallee <astronomemusic@gmail.com>
2014-12-04 21:57:45 -1000

    strikes and unstrikes selected item but continues to unwrap span element

--- public/js/todo.js
+++ public/js/todo.js
@@ -4,18 +4,19 @@ $( document ).ready(function() {
       var checkbox = $('<li><input type="checkbox"></input></li>');
       $('ul').append(checkbox);
       var user_input = $('#target').val();
-      $(checkbox).append(user_input);
-      // var checkbox = $('<input type="checkbox">');
-      // $('li').append(checkbox);
-      // resets to placeholder
+      $(checkbox).append('<span>' + user_input + '</span>');
+      // RESESTS TO PLACEHOLDER
       $('input').val('').removeAttr('checked').removeAttr('selected');
     }
     // CROSS OUT LIST ITEM IF BOX CHECKED
     $('input:checkbox').change(
       function() {
         if(this.checked) {
-          $('li').wrap('<strike>');
+          $(this).siblings().wrap('<strike>');
+        } else {
+          console.log("uncheck");
+          $('span').unwrap();
         }
-      })
+      });
   });  
 });
